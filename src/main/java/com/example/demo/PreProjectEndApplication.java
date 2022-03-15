917a5a97e28678adbe29f68b544239214fdbc671
вадим гаврилов <df_vadim_dfg@mail.ru>
2022-03-15 12:08:48 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/PreProjectEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProjectEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreProjectEndApplication.class, args);
+	}
+
+}
